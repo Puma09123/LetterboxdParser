b728127b1243a7c024c0ef899a521166f97b6942
Puma09123 <Rituss09@gmail.com>
2026-01-13 22:22:22 +0900

    Add parser module skeleton
    Implements a function stub for getting user ratings from Letterboxd.

---
+++ parser.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_user_ratings(username):
+    """
+    Получает имя пользователя Letterboxd и возвращает
+    список фильмов и пользовательских оценок.
+    """
+    pass
