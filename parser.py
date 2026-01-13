0964867ff88ee445a527956f229ca1599b4335e7
Puma09123 <Rituss09@gmail.com>
2026-01-13 22:29:40 +0900

    Add request to Letterboxd user page

--- parser.py
+++ parser.py
@@ -1,10 +1,12 @@
-import requests
-from bs4 import BeautifulSoup
-
-
 def get_user_ratings(username):
     """
     Получает имя пользователя Letterboxd и возвращает
-    список фильмов и пользовательских оценок.
+    HTML-страницу с оценками пользователя.
     """
-    pass
+    url = f"https://letterboxd.com/{username}/films/"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        raise Exception("Не удалось загрузить страницу пользователя")
+
+    return response.text
